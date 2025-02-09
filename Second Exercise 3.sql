9e9527cb010cb4487fbc0179fc67a700ab9ffe79
Miroslav Tzarev <mtzarev@gmail.com>
2025-02-09 19:06:43 +0200

    Second Exercises 3/4/5

---
+++ Second Exercise 3.sql
@@ -0,0 +1,21 @@
+use soft_uni;
+# Second Exercise 1
+
+select first_name, last_name from `employees`
+where substring(`first_name`, 1, 2) = 'Sa'
+order by `employee_id`;
+
+# Second Exercise 2
+
+select `first_name`from `employees`
+where `last_name` like '%ei%'
+order by `employee_id`;
+
+# Second Exercise 3
+
+select `first_name` from employees
+where `department_id` in (3, 10) 
+and year(`hire_date`) between 1995 and 2005
+order by `employee_id`;
+
+
