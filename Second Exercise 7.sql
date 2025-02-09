2b9de6c7a0c554b14aa03e5e616aa9c6767644c4
Miroslav Tsarev <mtzarev@gmail.com>
2025-02-09 23:41:17 +0200

    Update Second Exercise 7.sql

--- Second Exercise 7.sql
+++ Second Exercise 7.sql
@@ -3,7 +3,6 @@ use soft_uni;
 select first_name, last_name from `employees`
 where substring(`first_name`, 1, 2) = 'Sa'
 order by `employee_id`;
-
 # Second Exercise 2
 
 select `first_name`from `employees`
