8e529cc02923f60138166867e641fdf453fd8774
Miroslav Tsarev <mtzarev@gmail.com>
2025-02-09 23:41:25 +0200

    Update Second Exercise 7.sql

--- Second Exercise 7.sql
+++ Second Exercise 7.sql
@@ -4,7 +4,6 @@ select first_name, last_name from `employees`
 where substring(`first_name`, 1, 2) = 'Sa'
 order by `employee_id`;
 # Second Exercise 2
-
 select `first_name`from `employees`
 where `last_name` like '%ei%'
 order by `employee_id`;
